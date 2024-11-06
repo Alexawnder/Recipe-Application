06a1a457f6a89b9ef3ac879cd85cf42751edf055
Matthew Paredes <matthewparedes2k3@gmail.com>
2024-11-06 15:47:55 -0800

    Started recipe downloader

--- recipeapp/lib/RecipeAPI.dart
+++ recipeapp/lib/RecipeAPI.dart
@@ -1,6 +1,10 @@
 import 'dart:io' as io;
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+import 'apikey.dart';
+
 final String _RECIPE_CACHE_DIR = "cache/recipes/";
 
 class RecipeAPI{
@@ -9,9 +13,13 @@ class RecipeAPI{
     }
     static Map<String,dynamic> getRecipe(int id){
         io.File myFile = new io.File(_RECIPE_CACHE_DIR + id.toString() + ".json");
-        if(myFile.existsSync()){
-            return jsonDecode(myFile.readAsStringSync());
+        if(!myFile.existsSync()){
+            downloadRecipe(id);
         }
-        return new Map<String,dynamic>(); 
+        return jsonDecode(myFile.readAsStringSync());
+
+    }
+    static void downloadRecipe(int id) async{
+        await http.get("https://api.spoonacular.com/recipes/{id}/information?apiKey=" + apikey.API_KEY);
     }
 }
