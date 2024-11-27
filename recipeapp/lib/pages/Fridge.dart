a898483ee6d6deb94a4bbd41e05fb8d877a28086
Matthew Paredes <matthewparedes2k3@gmail.com>
2024-11-26 23:38:59 -0800

    Basic fridge list (untested)

--- recipeapp/lib/pages/Fridge.dart
+++ recipeapp/lib/pages/Fridge.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
-import 'types/Ingredient.dart';
+import '../types/ingredient.dart';
 class Fridge extends StatelessWidget {
 
-    List<Ingredient> fridgeContents;
+    List<Ingredient> fridgeContents = [];
     
     @override
     Widget build(BuildContext context) {
@@ -20,10 +20,22 @@ class Fridge extends StatelessWidget {
                     return Card(
                         margin: const EdgeInsets.all(8.0),
                         child: ListTile(
-                            leading: Image.network(fridgeItem
-
+                            leading: Image.network(fridgeItem.image,
+                                width:50, height: 50, fit: BoxFit.cover),
+                            title: Text(
+                                fridgeItem.name,
+                                style: GoogleFonts.dancingScript(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: const Color(0xFF7EA16B),
+                                ),
+                            ),
+                        ),
+                    );
+                }
+            ),
+        );
     }
-
 }
 
 
