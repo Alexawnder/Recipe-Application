5444fbf5cd434ba2596425968f69936231ffdd47
Matthew Paredes <matthewparedes2k3@gmail.com>
2024-11-30 22:37:08 -0800

    Fridge now uses provider

--- recipeapp/lib/pages/Fridge.dart
+++ recipeapp/lib/pages/Fridge.dart
@@ -1,39 +1,43 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
 import '../types/ingredient.dart';
-class Fridge extends StatelessWidget {
+import 'package:provider/provider.dart';
+import '../providers/FridgeContentsProvider.dart';
 
-    List<Ingredient> fridgeContents = [];
-    
+class Fridge extends StatelessWidget {
+ 
     @override
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
                 title: Text('My Fridge'),
             ), // AppBar
-            body: ListView.builder(
-                itemCount: fridgeContents.length,
-                itemBuilder: (context, index) {
-                    Ingredient fridgeItem = fridgeContents[index];
-                    return Card(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                            leading: Image.network(fridgeItem.image,
-                                width:50, height: 50, fit: BoxFit.cover),
-                            title: Text(
-                                fridgeItem.name,
-                                style: GoogleFonts.dancingScript(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: const Color(0xFF7EA16B),
+            body: Consumer<FridgeContentsProvider>(
+                builder: (context, fridgeContentsProvider, child) {
+                    Map<Ingredient, int> contents = fridgeContentsProvider.fridgeContents;
+                    return ListView.builder(
+                        itemCount: contents.length,
+                        itemBuilder: (context, index) {
+                            Ingredient fridgeItem = contents.keys.elementAt(index);
+                            return Card(
+                                margin: const EdgeInsets.all(8.0),
+                                child: ListTile(
+                                    leading: Image.network(fridgeItem.image,
+                                        width:50, height: 50, fit: BoxFit.cover),
+                                    title: Text(
+                                        fridgeItem.name,
+                                        style: GoogleFonts.dancingScript(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: const Color(0xFF7EA16B),
+                                        ),
+                                    ),
                                 ),
-                            ),
-                        ),
+                            );
+                        }
                     );
                 }
-            ),
+            )
         );
     }
 }
