452d2bc633dd5d90d355038f7cf0e298b2628143
Matthew Paredes <matthewparedes2k3@gmail.com>
2024-12-01 00:09:34 -0800

    FridgeContentsProvider now properly removes 0 quantity ingredients

--- recipeapp/lib/providers/FridgeContentsProvider.dart
+++ recipeapp/lib/providers/FridgeContentsProvider.dart
@@ -15,18 +15,21 @@ class FridgeContentsProvider with ChangeNotifier {
     void removeIngredient(Ingredient i){
         if(_fridgeContents.containsKey(i)){
             _fridgeContents.update(i, (value) => value - 1);
+            _fridgeContents.removeWhere((key, value) => value <= 0);
             notifyListeners();
         }
     }
     void removeAllIngredient(Ingredient i){
         if(_fridgeContents.containsKey(i)){
             _fridgeContents.remove(i);
+            _fridgeContents.removeWhere((key, value) => value <= 0);
             notifyListeners();
         }
     }
     void setIngredientQty(Ingredient i, int qty){
         if(_fridgeContents.containsKey(i)){
             _fridgeContents.update(i, (value) => qty);
+            _fridgeContents.removeWhere((key, value) => value <= 0);
             notifyListeners();
         }
     }
