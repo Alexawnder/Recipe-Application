cfb21ac8961588a364ad75449ffb04f5a31ded18
abigailxcal <acalde1997@gmail.com>
2024-12-01 18:24:29 -0800

    aesthetic changes

--- recipeapp/lib/pages/SavedRecipes.dart
+++ recipeapp/lib/pages/SavedRecipes.dart
@@ -4,17 +4,29 @@ import 'RecipeDetails.dart';
 
 class SavedRecipes extends StatelessWidget {
   final List<Map<String, dynamic>> savedRecipes;
-  final Function(Map<String, dynamic>) onSave; // callback to remove/unsave a recipe
-  const SavedRecipes({super.key, required this.savedRecipes,required this.onSave,});
-
-  
+  final Function(Map<String, dynamic>)
+      onSave; // callback to remove/unsave a recipe
+  const SavedRecipes({
+    super.key,
+    required this.savedRecipes,
+    required this.onSave,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Saved Recipes'),
+        title: Text(
+          'Saved Recipes',
+          style: GoogleFonts.playfairDisplay(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF000000),
+          ),
+        ),
+        backgroundColor: const Color(0xFF7EA16B),
       ),
+      backgroundColor: Color(0xFFF9DEC9),
       body: savedRecipes.isEmpty
           ? const Center(
               child: Text(
@@ -30,41 +42,43 @@ class SavedRecipes extends StatelessWidget {
                 final imageUrl = recipe['image'];
 
                 return Card(
-                  margin: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: Image.network(
-                      imageUrl,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
-                    ),
-                    title: Text(
-                      label,
-                      style: GoogleFonts.dancingScript(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: const Color(0xFF7EA16B),
+                    margin: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      tileColor: Color(0xFFC4A69D),
+                      leading: Image.network(
+                        imageUrl,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
                       ),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.red,
-                      ),
-                      onPressed: () {
-                        onSave(recipe); // triggers callback to remove the recipe
-                      },
-                    ),
-                      onTap: () {  // navigates to the recipe page of the corresp. recipe
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => RecipeDetails(recipe: recipe),
+                      title: Text(
+                        label,
+                        style: GoogleFonts.playfairDisplay(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFF000000),
                         ),
-                      );
-                    },
-                  )
-                );
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          onSave(
+                              recipe); // triggers callback to remove the recipe
+                        },
+                      ),
+                      onTap: () {
+                        // navigates to the recipe page of the corresp. recipe
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RecipeDetails(recipe: recipe),
+                          ),
+                        );
+                      },
+                    ));
               },
             ),
     );
